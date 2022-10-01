ec42c8ec7737b20bc67775f11a00a1e296a01946
Eduardo S. Libardi <eslibardi@gmail.com>
2022-10-01 14:38:57 -0300

    Fix: Move email port to env, and flex email server connection
    Some email servers use port 587, while other use port 25,
    moving email port to env.
    Some email servers may have self signed certification

--- utils/nodemailerConfig.js
+++ utils/nodemailerConfig.js
@@ -2,9 +2,12 @@ require('dotenv').config();
 
 module.exports = {
   host: process.env.EMAIL_HOST,
-  port: 587,
+  port: process.env.EMAIL_PORT,
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASSWORD,
+  },
+  tls: {
+    rejectUnauthorized: false,
   },
 };
